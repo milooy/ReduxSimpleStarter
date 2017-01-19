80229694c460b016ee3392253db89c2d9d87453f
jayjin <jayjinjay@gmail.com>
2017-01-19 15:12:37 +0900

    ~65

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -7,13 +7,15 @@ class WeatherList extends Component {
 	renderWeather(cityData) {
 		const name = cityData.city.name;
 		const temps = cityData.list.map(weather => weather.main.temp);
+		const pressures = cityData.list.map(weather => weather.main.pressure);
+		const humidities = cityData.list.map(weather => weather.main.humidity);
 
 		return (
 			<tr key={name}>
 				<td>{name}</td>
-				<td>
-					<Chart data={temps} color="orange" />
-				</td>
+				<td><Chart data={temps} color="orange" units="K"/></td>
+				<td><Chart data={pressures} color="green" units="hPa"/></td>
+				<td><Chart data={humidities} color="gray" units="%"/></td>
 			</tr>
 		)
 	}
@@ -24,9 +26,9 @@ class WeatherList extends Component {
 				<thead>
 					<tr>
 						<th>City</th>
-						<th>온도</th>
-						<th>기압</th>
-						<th>습도</th>
+						<th>온도(K)</th>
+						<th>기압(hPa)</th>
+						<th>습도(%)</th>
 					</tr>
 				</thead>
 				<tbody>
